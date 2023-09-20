62e770867b13ac364b3dbb44df907af6663e77ae
João Victor Santos Rodrigues <joaorodrigues@bne.com.br>
2023-09-20 19:50:47 -0300

    Oh gods, it was wayyyy better to use hash

--- src/group_anagram/group_anagram.cpp
+++ src/group_anagram/group_anagram.cpp
@@ -3,49 +3,55 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <fstream>
+#include <string>
 
 // Given an array of strings strs, group the anagrams together. You can return the answer in any order.
 // example input ["eat", "tea", "tan", "ate", "nat", "bat"]
 // expected output [["bat"],["nat","tan"],["ate","eat","tea"]]
 
-bool is_anagram(std::string s, std::string t) {
-    if (s.size() != t.size()) {
-        return false;
+// Function to generate a hash for a string
+std::string generateHash(const std::string& str) {
+    std::vector<int> count(26, 0); // Assuming lowercase English letters
+    for (char c : str) {
+        count[c - 'a']++;
     }
-    sort(s.begin(), s.end());
-    sort(t.begin(), t.end());
-    for (int i = 0; i < s.size(); i++) {
-        if (s[i] != t[i])
-            return false;
+    std::string hash;
+    for (int i = 0; i < 26; i++) {
+        hash += std::to_string(count[i]) + "#";
     }
-    return true;
+    return hash;
 }
 
 std::vector<std::vector<std::string>> groupAnagrams(std::vector<std::string>& strs) {
-    std::vector<std::vector<std::string>> result = {};
-    std::unordered_map<std::string, std::vector<std::string>> map = {};
-    for (int i = 0; i < strs.size(); i++) {
-        std::string s = std::string(strs[i]);
-        sort(s.begin(), s.end());
-        if (map.find(s) != map.end()) {
-            continue;
-        }
-        map.insert({s, {strs[i]}});
-        for (int z = i + 1; z < strs.size(); z++) {
-            std::string t = strs[z];
-            // if (map.find(t) != map.end())
-            //     continue;
-            if (is_anagram(strs[i], t)) {
-                map[s].push_back(t);
-            }
-        }
-        result.push_back(map[s]);
+    std::unordered_map<std::string, std::vector<std::string>> map;
+    std::vector<std::vector<std::string>> result;
+
+    for (const std::string& str : strs) {
+        std::string hash = generateHash(str);
+        map[hash].push_back(str);
     }
+
+    for (const auto& pair : map) {
+        result.push_back(pair.second);
+    }
+
     return result;
 }
 
 int main() {
-    std::vector<std::string> strs = {"eat","tea","tan","ate","nat","bat"};
+    // GODS!
+    std::ifstream file;
+    file.open("god-forsaken-text-file.txt", std::ios::in);
+    std::vector<std::string> strs;
+    if(file.is_open()){
+        std::string str;
+        while (std::getline(file, str, ','))
+        {
+            strs.emplace_back(str);
+        }
+    }
+    
     std::vector<std::vector<std::string>> expected_output = {{"bat"},{"nat","tan"},{"ate","eat","tea"}};
     auto vec = groupAnagrams(strs);
     const char* res = vec == expected_output ? "equal" : "not equal";
